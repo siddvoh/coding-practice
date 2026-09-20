85cd5815fedb1524fc3c3162ef80270dcdbc115b
Siddharth Vohra <49568132+siddvoh@users.noreply.github.com>
2026-09-19 21:33:20 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 1.1
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Sidd"
+print(type(variable))
+variable = ["Sidd", "Som"]
+print(type(variable))
