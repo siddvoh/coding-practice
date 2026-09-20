6252956dada2ba7d7b57f42e4b3de4a40096d9a4
Siddharth Vohra <49568132+siddvoh@users.noreply.github.com>
2026-09-19 21:32:32 -0400

    Add: python-variable-types - submission-1

---
+++ Python For Beginners/python-variable-types/submission-1.py
@@ -0,0 +1,13 @@
+
+integer_type = 2
+list_type = [1,2,3]
+float_type = 2.2
+string_type = "Sidd"
+bool_type = True
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
